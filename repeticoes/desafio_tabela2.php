741d4488f6014d686cbe01f6650d04e3d03a755b
Marcelo <marcelo_henrique.12@outlook.com>
2024-09-04 16:52:20 -0300

    vs_0409

--- repeticoes/desafio_tabela2.php
+++ repeticoes/desafio_tabela2.php
@@ -1,26 +1,40 @@
 <div class="titulo">Desafio Tabela II</div>
 
-<form action="#" method="post"></form>
+<form action="#" method="post">
     <div>
         <label for="linhas">Linhas</label>
-        <input type="number" value="10"
+        <input type="number" value=<?= $_POST['linhas'] ?? 10 ?>
            name="linhas" id="linhas">
     </div>
 
     <div>
         <label for="colunas">Colunas</label>
-        <input type="number" value="10" 
+        <input type="number" value=<?= $_POST['colunas'] ?? 10 ?>
            name="colunas" id="colunas">
     </div>
 
         <button>Gerar Tabela</button>
-
 </form>
 
+<table>
 <?php
     $linhas = intval($_POST['linhas']);
     $colunas = intval($_POST['colunas']);
+
+    if(!$linhas) $linhas = 10;
+    if(!$colunas) $colunas = 10;
+
+        $num = 1;
+        for($i = 0; $i < $linhas; $i++) {
+            echo "<tr>";
+            for($j = 0; $j < $colunas; $j++) {
+             echo "<td>$num</td>";
+             $num++;
+            }  
+            echo "</tr>";
+    }
 ?>
+</table>
 
 
 
