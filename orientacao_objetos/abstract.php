630f98b30c498af4b73ed4a00cfe905b1c2c0d4d
Marcelo Magalhães <celoxd01@gmail.com>
2024-11-10 20:35:10 -0300

    10.11

--- orientacao_objetos/abstract.php
+++ orientacao_objetos/abstract.php
@@ -2,3 +2,37 @@
 
 <?php
 
+abstract class Abstrata {  // Não pode ser utilizada de forma primária, sendo assim, os métodos só serão extendidos após a utilização da Classe CONCRETA
+    abstract public function metodo1(); 
+    abstract protected function metodo2($parametro);
+}
+
+abstract class FilhaAbstrata extends Abstrata { // Classe filha abstrata que se extende da primária
+    public function metodo1() {
+        echo "Executando método 1<br>";
+    }
+    abstract public function metodo3(); 
+}
+
+class Concreta extends FilhaAbstrata { 
+    public function metodo1() { 
+        echo "Executando método 1 extendido <br>";
+        parent::metodo1();
+    }
+    protected function metodo2($parametro){ 
+        echo "Executando método 2, com parâmetro $parametro";
+    }
+    public function metodo3() { 
+        echo "Executando método 3<br>";
+        $this->metodo2('interno');
+    }
+}
+
+$c = new Concreta; 
+$c->metodo1(); 
+// $c->metodo2('externo');
+$c->metodo3();
+
+echo '<br>';
+echo "Código validado!";
+
